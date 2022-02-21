a0975b64ab698bbc83015a56b9eee9dd219588bc
carlogilmar <carlogilmar12@gmail.com>
2022-02-21 12:31:31 -0600

    Update conf

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-echo "Actualizando blog"
+echo " == Actualizando blog =="
 rm -rf docs/
+echo "1. Generando todo el sitio"
 hugo -D
+echo "2. Renombrando folder"
 mv public/ docs/
-echo "Donas...!"
+echo "Listo..."
+echo "3. Realizando commit"
+git add .
+git commit -m "Sitio rebuild DONE"
+
